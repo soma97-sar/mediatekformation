b0edaf9ce74d3483ad02e248c346e32b8d74f498
BEN BAHA <soumia.azza.91.97@gmail.com>
2023-03-23 09:51:37 +0100

    phase 16:l’ajout d’un accès avec authentification a la back office en utilisant keycloak

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,26 +7,38 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
-    #[ORM\Column(length: 180, unique: true)]
-    private ?string $email = null;
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
 
-    #[ORM\Column]
-    private array $roles = [];
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
 
     /**
      * @var string The hashed password
+     * @ORM\Column(type="string")
      */
-    #[ORM\Column]
-    private ?string $password = null;
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $keycloakId;
 
     public function getId(): ?int
     {
@@ -115,5 +127,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getKeycloakId(): ?string
+    {
+        return $this->keycloakId;
+    }
+
+    public function setKeycloakId(?string $keycloakId): self
+    {
+        $this->keycloakId = $keycloakId;
+
+        return $this;
     }
 }
---
+++ tests/FormationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\tests;
+
+use App\Entity\Formation;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of FormationTest
+ *
+ * @author BEN BAHA
+ */
+class FormationTest extends TestCase{
+    public function testgetPublishedAtString() {
+        $formation = new Formation();
+        $formation->setPublishedAt(new DateTime("2021-01-04"));
+        $this->assertEquals("04/01/2021", $formation->getPublishedAtString());
+        
+    }
+}
